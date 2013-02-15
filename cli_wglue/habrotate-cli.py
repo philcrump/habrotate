7ccf651da5f74ef165a888768ccd29e4755cbe0e
Phil Crump <pdc1g09@soton.ac.uk>
2013-02-15 23:04:06 +0000

    Set our rotator bearing each loop, to correct for user twiddling.

--- cli_wglue/habrotate-cli.py
+++ cli_wglue/habrotate-cli.py
@@ -128,10 +128,10 @@ try:
 			if update_rotator == 1:
 				update_rotator = 0
 				print("Moving rotator to Azimuth: " + str(rotator_bearing) + " Elevation: " + str(rotator_elevation))
-				udp_string = "<PST><TRACK>0</TRACK><AZIMUTH>" + str(rotator_bearing) + "</AZIMUTH><ELEVATION>" + str(rotator_elevation) + "</ELEVATION></PST>"
-				udp_socket.sendto(udp_string, udp_config)
 			else:
 				print ("Current Rotator error: Azimuth: " + str(rotator_bearing-bearing) + " Elevation: " + str(rotator_elevation-elevation))
+			udp_string = "<PST><TRACK>0</TRACK><AZIMUTH>" + str(rotator_bearing) + "</AZIMUTH><ELEVATION>" + str(rotator_elevation) + "</ELEVATION></PST>"
+			udp_socket.sendto(udp_string, udp_config)
 		time.sleep(10)
 		loopcount+=1
 except KeyboardInterrupt:
